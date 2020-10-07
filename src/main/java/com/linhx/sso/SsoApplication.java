a8de106e89aa2ac5b112ee144d9a307791ce5619
linhx <nguyendinhlinhx@gmail.com>
2020-10-07 19:53:18 +0700

    init project

---
+++ src/main/java/com/linhx/sso/SsoApplication.java
@@ -0,0 +1,11 @@
+package com.linhx.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SsoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/linhx/sso/SsoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.linhx.sso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
